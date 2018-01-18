1cd340ba248d3a106a55d348fc19b9dfaf6e22db
Guest User <Guest@Epicodus-10D.local>
2018-01-18 10:23:13 -0800

    add functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
-$(document).ready(function() {
-  $("form#leap-year").submit(function(event) {
-    event.preventDefault();
-    var year = parseInt($("input#year").val());
-    var result = leapYear(year);
-    $("#result").text(result);
-  });
-});
-
 var leapYear = function(year) {
   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
     return true;
@@ -14,3 +5,22 @@ var leapYear = function(year) {
     return false;
   }
 };
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
+  });
+});
